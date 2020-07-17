3ca9833759014f2b51672c35abe4cfef78dc74ec
Ryan <>
2020-07-17 11:22:26 -0400

    Create Noise Module. Now to add the algorithms

---
+++ EEXT_Noise/src/java/engine/noise/EEXT_Noise.java
@@ -0,0 +1,42 @@
+package engine.noise;
+
+import engine.Extension;
+
+public class EEXT_Noise extends Extension
+{
+    @Override
+    public void beforeSetup()
+    {
+    
+    }
+    
+    @Override
+    public void afterSetup()
+    {
+    
+    }
+    
+    @Override
+    public void beforeDraw(double elapsedTime)
+    {
+    
+    }
+    
+    @Override
+    public void afterDraw(double elapsedTime)
+    {
+    
+    }
+    
+    @Override
+    public void beforeDestroy()
+    {
+    
+    }
+    
+    @Override
+    public void afterDestroy()
+    {
+    
+    }
+}
